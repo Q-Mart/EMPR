6ebfa5a60a6a3ee0555d61f1982ef1967750b659
Ben Simner <bs829@york.ac.uk>
2016-01-11 15:58:40 +0000

    Add example

--- src/main.c
+++ src/main.c
@@ -1,4 +1,32 @@
-int main(){
-  while(1);
-  return 1;
+#include "keypad.h"
+#include "lcd.h"
+#include "lpc17xx_gpio.h"
+
+int main(void)
+{
+    lcd_init();
+    lcd_clear_display();
+
+    keypad_init();
+    keypad_enable_int();
+
+    while (1);
+}
+
+void EINT3_IRQHandler(void) 
+{
+    static int state = 0;
+
+    if (GPIO_GetIntStatus(0, 23, 1))
+    { 
+        keypad_clear_int();
+        
+        if (state == 0) {
+            lcd_send_str(0x00, "Interrupt!");
+            state = 1;
+        } else {
+            lcd_send_str(0x00, "Reset!");
+            state = 0;
+        }
+    }
 }
