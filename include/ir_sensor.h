87ea78903b0892cbfd77da3d5652820bb4b1e4d2
Oliver Downard <olivermd@btinternet.com>
2016-01-12 11:16:49 +0000

    Added the ir_sensor library and spruced it up

---
+++ include/ir_sensor.h
@@ -0,0 +1,9 @@
+#include "adc.h"
+#ifndef IR_SENSOR
+#define IR_SENSOR
+void ir_sensor_init();
+void ir_sensor_set_far_point(float x); //20cm
+void ir_sensor_set_near_point(float x); //10cm
+void ir_sensor_calibrate(); //Uses near and far values to calibrate
+float ir_sensor_get_distance();
+#endif
