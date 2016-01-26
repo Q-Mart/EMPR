0f8337d66bcaee98642f69373c960b6d383c46d6
Oliver Downard <olivermd@btinternet.com>
2016-01-26 11:56:58 +0000

    BANTER

--- include/lcd.h
+++ include/lcd.h
@@ -9,8 +9,7 @@
 void lcd_init(void);
 void lcd_wait_while_busy(void);
 void lcd_clear_display(void);
-void lcd_send_char(uint8_t, char);
-void lcd_send_str(uint8_t, char*);
-void lcd_send_pat(uint8_t, char);
+
 void lcd_send_lines(char*, char*);
+void lcd_send_line(uint8_t, char*);
 void lcd_send_strf(uint8_t, char*, ...);
