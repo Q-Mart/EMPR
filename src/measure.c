80d3dd3ccf3e51edaf38d0a5b4aad463a0d68bba
Oliver Downard <olivermd@btinternet.com>
2016-02-01 16:19:29 +0000

    Changed states to be removed from main file so other files can use it

--- src/measure.c
+++ src/measure.c
@@ -2,11 +2,12 @@
 #include "lcd.h"
 #include "ir_sensor.h"
 #include "debug.h"
+#include "state.h"
 void any_to_measure(){
     lcd_send_line(LINE1, "Measure Mode");
 }
 void measure_loop(){
-    uint8_t measure_byte = 0x02;
+    uint8_t measure_byte = MEASURE_DO;
     uint32_t raw = ir_sensor_get_raw_data();
     debug_send_arb((char*) &measure_byte, 1);
     debug_send_arb((char*) &raw, 4);
