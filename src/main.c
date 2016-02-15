d1cd74ebece6a0b297dd032872073c4e86336281
icydoge <icydoge@gmail.com>
2016-02-15 15:16:07 +0000

    prepare to merge from master

--- src/main.c
+++ src/main.c
@@ -1,183 +1,108 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lpc17xx_timer.h"
+#include "debug.h"
 #include "keypad.h"
 #include "lcd.h"
-#include "debug.h"
-#include "gpio.h"
-#include "timer.h"
-#include "ultrasound.h"
-#include "servo.h"
+#include "lpc17xx_gpio.h"
+#include "test.h"
+#include "calibration_mode.h"
+#include "multi.h"
+#include "scan.h"
+#include "measure.h"
+#include "adc.h"
+#include "state.h"
 
-/* Instructions to use ultrasound module:
-    Connect Echo pin of the sensor to P29 on IO Board;
-    Connect Trigger pin of the sensor to P8 on IO Board;
-    Call initialise_timer_measurement() at start;
-    Now calibrate the sensor, calculated the speed of sound
-     as it appears with the calibration object, in m/s, save
-     in calibrated_gradient;
-    Call send_test_pulse() to send a pulse, then immediately:
-    call process_ultrasound_value(calibrated_gradient,\
-        (int)ultrasound_valid_response_time) 
-     to obtain object distance in cm.
-*/
-int tracker_full_scan(int, int, int, uint32_t*);
-void tracker_narrow_sweep();
-int tracker_find_smallest_index(uint32_t*, int, int);
-void tracker_set_bound();
-int tracker_compare_indices(int, int);
-
-int tracker_upper_bound = 0;
-int tracker_lower_bound = 0;
-int tracker_current_center = 0;
-
+static int current_state_input = 0;
+static state_t current_state = CALIBRATE;
+void state_transition(char key);
 int main(void)
 {
+    /*
+      The order of initialisation matters!
+     */
+    timer_enable_systick();
+    adc_enable();
     debug_init();
-
     lcd_init();
     lcd_clear_display();
-    
-    ultrasound_initialise_timer_measurement();
+    keypad_init();
+    keypad_enable_int();
+    ir_sensorInit();
     servo_init();
-
-    uint32_t tracker_ultrasound_range_table[32], tracker_ir_range_table[32];
-    int tracker_ultrasound_range_index, tracker_ir_range_index;
-
-    tracker_ultrasound_range_index = tracker_full_scan(0, 250, 10, tracker_ultrasound_range_table);
-    tracker_current_center = tracker_find_smallest_index(tracker_ultrasound_range_table, tracker_ultrasound_range_index, 2) * 10;
-    servo_set_pos(tracker_current_center);
-    tracker_set_bound();
-
-/*
-    char print_distance[80];
-    int ii;
-    for (ii = 0; ii < 25; ii++) {
-        debug_sendfc("%d :", ii);
-        sprintf(print_distance, "Value: %d \r\n", tracker_ultrasound_range_table[ii]);
-        debug_send(print_distance);
-    }
-    debug_sendfc("Center: %d\r\n", current_center); */
+    any_to_calib();
 
     while (1)
     {
-        tracker_narrow_sweep();
-        tracker_set_bound();
-        timer_delay(125);
-    }
-}
-
-int tracker_full_scan(int start, int end, int increment, uint32_t * tracker_ultrasound_range_table)
-{
-    int sensor_position;
-    int range_index = 0;
-    static uint32_t measured_distance;
-    char print_distance[64];
-    debug_send("===Beginning full scan.===\r\n");
-    for (sensor_position = start; sensor_position <= end; sensor_position += increment)
-    {
-        servo_set_pos(sensor_position);
-        timer_delay(375);
-        ultrasound_send_test_pulse();
-        measured_distance = ultrasound_process_value(340, 0, ultrasound_valid_response_time);
-        sprintf(print_distance, "Distance: %lu um\r\n\r\n", measured_distance);
-        debug_send(print_distance);
-        tracker_ultrasound_range_table[range_index] = measured_distance;
-        range_index++;
-    }
-    debug_send("===Full scan complete.===\r\n");
-    return range_index;
-}
-
-/* Narrow sweep within a cone of +-20 degrees, returns the new center */
-void tracker_narrow_sweep()
-{
-    int range_index, new_index, scan_start, scan_end, sensor_position;
-
-    static uint32_t measured_distance;
-    uint32_t tracker_ultrasound_range_table[15];
-    int tracker_ultrasound_index_table[15];
-
-    if (abs(tracker_lower_bound - servo_get_pos()) < abs(tracker_upper_bound - servo_get_pos()))
-    {
-        scan_start = tracker_lower_bound;
-        scan_end = tracker_upper_bound;
-    }
-    else {
-        scan_start = tracker_upper_bound;
-        scan_end = tracker_lower_bound;
-    }
-
-    if (scan_start <= scan_end) {
-        range_index = 0;
-        for (sensor_position = scan_start; sensor_position <= scan_end; sensor_position += 5) {
-            servo_set_pos(sensor_position);
-            timer_delay(75);
-            ultrasound_send_test_pulse();
-            measured_distance = ultrasound_process_value(340, 0, ultrasound_valid_response_time);
-            tracker_ultrasound_range_table[range_index] = measured_distance;
-            tracker_ultrasound_index_table[range_index] = range_index - 6;
-            range_index++;
+        switch (current_state) {
+            case CALIBRATE:
+                break;
+            case SCAN:
+                scan_loop();
+                break;
+            case MEASURE:
+                measure_loop();
+                break;
+            case MULTI:
+                break;
+            default:
+        break;
         }
     }
-    else {
-        range_index = 0;
-        for (sensor_position = scan_start; sensor_position >= scan_end; sensor_position -= 5) {
-            servo_set_pos(sensor_position);
-            timer_delay(75);
-            ultrasound_send_test_pulse();
-            measured_distance = ultrasound_process_value(340, 0, ultrasound_valid_response_time);
-            tracker_ultrasound_range_table[range_index] = measured_distance;
-            tracker_ultrasound_index_table[range_index] = -1 * (range_index - 6);
-            range_index++;
+}
+
+void EINT3_IRQHandler(void) 
+{
+    if (GPIO_GetIntStatus(0, 23, 1))
+    { 
+        keypad_clear_int();
+
+        char r[16] = {0};
+        get_keyboard_presses(r);
+
+        int i;
+        for(i = 0; i < 16; ++i){
+            if(r[i] == 1) {
+                state_transition(KEYS[i]);
+            }
         }
     }
-    
-    new_index = tracker_find_smallest_index(tracker_ultrasound_range_table, range_index, 0);
-    tracker_current_center = (tracker_upper_bound + tracker_lower_bound) / 2 + 5 * tracker_ultrasound_index_table[new_index];
-    debug_sendf("The sensor thinks that the object is at %d degrees.\r\n", tracker_current_center);
 }
 
-void tracker_set_bound()
-{
-    if (tracker_current_center >= 240)
-    {
-        tracker_upper_bound = 270;
-        tracker_lower_bound = 210;
-    }
-    else if (tracker_current_center <= 30) {
-        tracker_lower_bound = 0;
-        tracker_upper_bound = 60;
-    }
-    else {
-        tracker_lower_bound = tracker_current_center - 30;
-        tracker_upper_bound = tracker_current_center + 30;
-    }
-}
+/* Transition function */
 
-int tracker_compare_indices(int ultrasound_index, int ir_index)
-{
-    if (abs(ultrasound_index - ir_index) >= 3)
-    {
-        return ir_index;
-    }
-    else {
-        return ((ultrasound_index + ir_index) / 2);
-    }
-}
+typedef void (*side_func)(void);
 
-int tracker_find_smallest_index(uint32_t darray[], int length, int initial)
+typedef struct
 {
-    /* Optionally ignore first initial number of values, due to motor still moving at the times of measurements. */
-    int smallest_index = initial;
-    uint32_t smallest = darray[initial];
+    state_t current;
+    char symbol;
+    state_t next;
+    side_func effect;
+} transition_t;
+
+const transition_t lut[] = {
+    {CALIBRATE_DONE, '#', CALIBRATE, NULL},
+    {CALIBRATE, '#', CALIBRATE_NEAR_DONE, &calib_to_near_calib},
+    {CALIBRATE_NEAR_DONE, '#', CALIBRATE_DONE, &near_calib_to_done},
+    {SCAN, '#', SCAN_DO, NULL},
+    {MEASURE, '#', MEASURE_DO, NULL},
+    {MULTI, '#', MULTI_DO_STAGE_1, NULL},
+    /* MAYBE DO THIS AUTOMATICALLY OR HAVE A WAIT? */
+    {MULTI_DO_STAGE_1, '#', MULTI_DO_STAGE_2, NULL},
+    {MULTI_DO_STAGE_2, '#', MULTI_DO_STAGE_3, NULL},
+    {MULTI_DO_STAGE_3, '#', MULTI_DO_STAGE_4, NULL},
+    {ANY, 'A', CALIBRATE, &any_to_calib},
+    {ANY, 'B', SCAN, &any_to_scan},
+    {ANY, 'C', MEASURE, &any_to_measure},
+    {ANY, 'D', MULTI, &any_to_multi}
+};
+void state_transition(char key){
+    /* global transitions from any state back to top-level ones */
     int i;
-    for (i = (initial+1); i < length; i++) {
-        if ((darray[i] < smallest) && (darray[i] > 20000)) {
-            /* Ignore values smaller than 2, due to unreliable values under the measurable range.*/
-            smallest_index = i;
-            smallest = darray[i];
+    for(i = 0; i < sizeof(lut)/sizeof(lut[0]); i++){
+        if ((lut[i].current == current_state || lut[i].current == ANY) && lut[i].symbol == key){
+        lcd_clear_display();
+        if(lut[i].effect !=NULL) (*(lut[i].effect))();
+        current_state = lut[i].next;
+        return;
         }
     }
-    return smallest_index;
 }
