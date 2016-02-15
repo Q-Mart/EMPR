8e5dfdd50c2ec91271c027d6843fd0be03d99df9
Oliver Downard <olivermd@btinternet.com>
2016-02-15 17:15:10 +0000

    Fixed indentation

--- src/measure.c
+++ src/measure.c
@@ -12,10 +12,10 @@ void any_to_measure() {
 
 void measure_loop() {
     uint32_t raw_ir = ir_sensor_get_raw_data();
-	ultrasound_send_test_pulse();
+    ultrasound_send_test_pulse();
     uint32_t dist_ir = ir_convert_to_distance(raw_ir);
-	uint32_t dist_us = ultrasound_get_distance(); 
-	uint32_t dist = (dist_ir + dist_us)/2;
+    uint32_t dist_us = ultrasound_get_distance(); 
+    uint32_t dist = (dist_ir + dist_us)/2;
     network_send(MEASURE_DO, (uint8_t *)&dist, 4, NULL);
     lcd_send_line(LINE2, "%d", dist);
 }
