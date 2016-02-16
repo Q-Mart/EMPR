fc25a2f0dcc51aecbe8fe7750d039723e1bc8858
Oliver Downard <omd505@york.ac.uk>
2016-02-16 11:45:35 +0000

    Used fixed length types, added mechanism to change how quickly the beep pulses

--- src/sound.c
+++ src/sound.c
@@ -6,20 +6,27 @@
 
 #define PI 3.14159265359
 
-static volatile int count;
-static const int max = 256;
-static const float max_amplitude = 511.0f;
-static const int amplitude_offset = 512;
+static volatile uint16_t sound_count;
+static const uint16_t sound_max = 256;
+static const float sound_max_amplitude = 511.0f;
+static const uint16_t sound_amplitude_offset = 512;
+static uint16_t sound_count_rate = 1;
 
 void sound_init(void){
-        count = 0;
+        sound_count = 0;
         dac_init();
         timer_set_systick_function(&sound_tick);
         timer_enable_systick();
 }
 
 void sound_tick(void){
-        int val = (sin((((double)count)/((double)max))*PI*2) * max_amplitude) + amplitude_offset;
+        int val = (sin((((double)sound_count)/((double)sound_max))*PI*2) * sound_max_amplitude) + sound_amplitude_offset;
         dac_update(val);
-        count = (count + 1) % max;
+        sound_count = (sound_count + sound_count_rate) % sound_max;
+}
+void sound_change_count_rate(uint16_t new_value){
+	sound_count_rate = new_value;
+}
+uint16_t sound_get_count_rate(void){
+	return sound_count_rate;
 }
--- src/sound.h
+++ src/sound.h
@@ -1,2 +1,6 @@
+#include "empr.h"
 void sound_init(void);
 void sound_tick(void);
+void sound_change_count_rate(int);
+void sound_change_count_rate(uint16_t);
+uint16_t sound_get_count_rate(void);
