95687bafbd2ea63f9c25356b67a8b3266b965340
Oliver Downard <omd505@york.ac.uk>
2016-02-16 10:42:22 +0000

    Added ultrasound to scan function

--- src/scan.c
+++ src/scan.c
@@ -3,6 +3,7 @@
 #include "lcd.h"
 #include "debug.h"
 #include "ir_sensor.h"
+#include "ultrasound.h"
 signed int scan_direction = 1;
 
 void any_to_scan(){
@@ -18,6 +19,8 @@ void scan_loop(){
     servo_set_pos(pos + (scan_direction));
     timer_delay(35);
     uint32_t raw = ir_sensor_get_raw_data();
+    uint32_t raw_us = ultrasound_get_distance();
+    raw = (raw + raw_us) /2;
     debug_send_arb((char*) &pos, 4);
     debug_send_arb((char*) &raw, 4);
     lcd_send_line(LINE2, "%d", ir_convert_to_distance(raw));
