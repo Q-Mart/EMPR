985c07f7002ce6a44e9b550d4ccdfb2b222d4cd0
Oliver Downard <omd505@york.ac.uk>
2016-02-20 15:07:39 +0000

    Update main.c
    Added a comment to explain the order of functions in input_poll().

--- src/main.c
+++ src/main.c
@@ -91,6 +91,9 @@ void input_poll(void){
     int i;
     for(i = 0; i < 16; ++i){
         if(r[i] == 1) {
+            //The order of these matters because otherwise the 
+            //key pressed for the transition will also register as
+            //an input for the corresponding loop function.
             last_key_press = key_to_int(KEYS[i]);
             state_transition(KEYS[i]);
         }
