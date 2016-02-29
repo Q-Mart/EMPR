dfec1c50f8e499f34ff5fe132f787a36b4fe9740
icydoge <icydoge@gmail.com>
2016-02-29 17:15:50 +0000

    Bias mode, not working yet

--- src/tracker.c
+++ src/tracker.c
@@ -3,7 +3,9 @@
 int tracker_upper_bound = 0;
 int tracker_lower_bound = 0;
 int tracker_current_center = 0;
-const int averaged_values_bearings[] = {-25, -15, -5, 5, 15, 25};
+int tracker_current_bias = 0;
+int tracker_current_measurements[TRACKER_MEASUREMENT_AMOUNT];
+int tracker_averaged_values_bearings[TRACKER_MEASUREMENT_AMOUNT-1] = {-25, -15, -5, 5, 15, 25};
 
 void tracker(void)
 {
@@ -24,16 +26,17 @@ void tracker(void)
         ) * 10;
 
     servo_set_pos(tracker_current_center);
-    tracker_set_bound();
+    tracker_current_center = tracker_set_bound(tracker_current_center);
 
     while (1)
     {
         tracker_narrow_sweep();
-        tracker_set_bound();
+        tracker_current_center = tracker_set_bound(tracker_current_center);
         timer_delay(35);
     }
 }
 
+/* Perform a full scan to find the initial object. */
 int tracker_full_scan(int start, int end, int increment, uint32_t * tracker_ultrasound_range_table, uint32_t * tracker_ir_range_table)
 {
     int sensor_position;
@@ -62,42 +65,23 @@ int tracker_full_scan(int start, int end, int increment, uint32_t * tracker_ultr
 /* Narrow sweep within a cone of +-30 degrees, returns the new center */
 void tracker_narrow_sweep()
 {
-    int range_index, new_index, scan_start, scan_end, sensor_position, x;
+    int tracker_previous_center = tracker_current_center;
+    int range_index, new_index, sensor_position, x;
     char lcd_string[20];
 
     static uint32_t tracker_ultrasound_range_table[7];
     static uint32_t tracker_ir_range_table[7];
 
-    //Set boundaries for sweep.
-    if (abs(tracker_lower_bound - servo_get_pos()) < abs(tracker_upper_bound - servo_get_pos()))
-    {
-        scan_start = tracker_lower_bound;
-        scan_end = tracker_upper_bound;
-    }
-    else {
-        scan_start = tracker_upper_bound;
-        scan_end = tracker_lower_bound;
-    }
-    tracker_current_center = (tracker_upper_bound + tracker_lower_bound) / 2;
-
-
     //Move servo to the current object direction.
-    //servo_set_pos(tracker_current_center);
+    servo_set_pos(tracker_current_center);
 
     //Perform narrow sweep.
-    debug_sendf("Starting at %d, Finishing at %d \r\n", scan_start, scan_end);
+    debug_sendf("Starting at %d, Finishing at %d \r\n", tracker_lower_bound, tracker_upper_bound);
     range_index = 0;
-    if (scan_start <= scan_end) {
-        for (sensor_position = scan_start; sensor_position <= scan_end; sensor_position += 10) {
-            tracker_narrow_sweep_scan(range_index, sensor_position, tracker_ultrasound_range_table, tracker_ir_range_table);
-            range_index++;
-        }
-    }
-    else {
-        for (sensor_position = scan_start; sensor_position >= scan_end; sensor_position -= 10) {
-            tracker_narrow_sweep_scan(range_index, sensor_position, tracker_ultrasound_range_table, tracker_ir_range_table);
-            range_index++;
-        }
+    for (x = 0; x < TRACKER_MEASUREMENT_AMOUNT; x++) {
+        sensor_position = tracker_current_measurements[x];
+        tracker_narrow_sweep_scan(range_index, sensor_position, tracker_ultrasound_range_table, tracker_ir_range_table);
+        range_index++;
     }
     
     //Find the angle with the smallest distance.
@@ -105,7 +89,18 @@ void tracker_narrow_sweep()
         tracker_process_range_table(tracker_ultrasound_range_table, range_index),
         tracker_process_range_table(tracker_ir_range_table, range_index) 
         );
-    tracker_current_center += averaged_values_bearings[new_index];
+    debug_sendf("tracker_averaged_values_bearings[new_index] = %d\r\n", tracker_averaged_values_bearings[new_index]);
+    tracker_current_center += tracker_averaged_values_bearings[new_index];
+
+    if (tracker_current_center > tracker_previous_center) {
+        tracker_current_bias = 1;
+    } else if (tracker_current_center == tracker_previous_center) {
+        tracker_current_bias = 0;
+    } else {
+        tracker_current_bias = -1;
+    }
+
+    debug_sendf("New bias: %d \r\n", tracker_current_bias);
 
     sprintf(lcd_string, "At %d degrees", tracker_current_center);
     lcd_send_line(LINE2, lcd_string);
@@ -117,6 +112,7 @@ void tracker_narrow_sweep()
     }
 }
 
+/* Peform one pass of the narrow scan on both sensors. */
 void tracker_narrow_sweep_scan(int range_index, int sensor_position, uint32_t * tracker_ultrasound_range_table, uint32_t * tracker_ir_range_table)
 {
     
@@ -133,13 +129,12 @@ void tracker_narrow_sweep_scan(int range_index, int sensor_position, uint32_t * 
 
 }
 
+/* Average two neighbouring values in the result. */
 int tracker_process_range_table(uint32_t * range_table, int range_table_size) {
 
     int i;                                
     uint32_t averaged_table[range_table_size - 1];
-    /* range_table at   -30 -20 -10  0  10  20  30 degrees 
-       averaged_table at -25, -15, -5, 5, 15, 25 degrees, taking 2 closest values in range_table
-    */
+
     for (i = 0; i < (range_table_size - 1); i++) {
         averaged_table[i] = (range_table[i] + range_table[i+1]) / 2;
         debug_sendf("%d: %lu, ", i, averaged_table[i]);
@@ -148,33 +143,58 @@ int tracker_process_range_table(uint32_t * range_table, int range_table_size) {
     return tracker_find_smallest_index(averaged_table, (range_table_size - 1), 0);
 }
 
-//Todo: add a directional bias to tracker_set_bound on Monday.
-void tracker_set_bound()
+/* Set scan boundaries. */
+int tracker_set_bound(current_center)
 {
+    int i;
 
-    if (tracker_current_center >= 240)
-    {
-        tracker_upper_bound = 255;
-        tracker_lower_bound = 195;
+    tracker_lower_bound = current_center - 30 + tracker_current_bias * 15;
+    tracker_upper_bound = current_center + 30 + tracker_current_bias * 15;
+
+    if ((tracker_upper_bound - tracker_lower_bound) != 60) {
+        debug_sendf("upper_bound, lower_bound error!\r\n");
     }
-    else if (tracker_current_center <= 30) {
+
+    if (tracker_lower_bound <= 15) {
+        tracker_upper_bound += abs(15 - tracker_lower_bound);
         tracker_lower_bound = 15;
-        tracker_upper_bound = 75;
     }
-    else {
-        tracker_lower_bound = tracker_current_center - 30;
-        tracker_upper_bound = tracker_current_center + 30;
+
+    if (tracker_upper_bound >= 255) {
+        tracker_lower_bound -= abs(tracker_upper_bound - 255);
+        tracker_upper_bound = 255;
     }
+
+    for (i = 0; i < TRACKER_MEASUREMENT_AMOUNT; i++) {
+        tracker_current_measurements[i] = tracker_lower_bound + 10 * i;
+    }
+
+    for (i = 0; i < (TRACKER_MEASUREMENT_AMOUNT - 1); i++) {
+        tracker_averaged_values_bearings[i] = -25 + 10 * i + 15 * tracker_current_bias;
+    }
+
+    current_center = (tracker_upper_bound + tracker_lower_bound) / 2;
+    
+    debug_sendf("tracker_set_bound: current_center = %d\r\n", current_center);
+    
+    return current_center;
+
 }
 
+/* Take the average of two sensors' indices. */
 int tracker_compare_indices(int ultrasound_index, int ir_index)
 {
     debug_sendf("U: %d \r\n", ultrasound_index);
     debug_sendf("I: %d \r\n", ir_index);
-        
+    
+    if (abs(ultrasound_index - ir_index)> 4) {
+        return ir_index;
+    }
+
     return ((ultrasound_index + ir_index) / 2);
 }
 
+/* Find the index of the smallest element in an array. */
 int tracker_find_smallest_index(uint32_t darray[], int length, int initial)
 {
     /* Optionally ignore first initial number of values, due to motor still moving at the times of measurements. */
@@ -191,6 +211,7 @@ int tracker_find_smallest_index(uint32_t darray[], int length, int initial)
     return smallest_index;
 }
 
+/* For LCD display. */
 void any_to_track(){
     lcd_send_line(LINE1, "Narrow Sweep");
     lcd_send_line(LINE2, "Tracking Object");
