92191c03d836dae1d1bbcc1f3c9a04c396c68e74
Oliver Downard <omd505@york.ac.uk>
2016-02-29 14:08:17 +0000

    Added leds to flash

--- src/sound.c
+++ src/sound.c
@@ -3,6 +3,7 @@
 #include "timer.h"
 #include <math.h>
 #include "debug.h"
+#include "leds.h"
 
 #define PI 3.14159265359
 
@@ -15,6 +16,7 @@ static int sound_enabled = 0;
 
 void sound_init(void){
         sound_count = 0;
+        init_gpio();
         dac_init();
         timer_set_systick_function(&sound_tick);
         timer_enable_systick();
@@ -31,6 +33,11 @@ void sound_tick(void){
         int val = (sin((((double)sound_count)/((double)sound_max))*PI*2) * sound_max_amplitude) + sound_amplitude_offset;
         dac_update(val);
         sound_count = (sound_count + sound_count_rate) % sound_max;
+        if(cound_count >= (sound_max - sound_count_rate)){
+            set_gpio(LED1 | LED2 | LED3 | LED4, 1);
+        }else{
+            set_gpio(LED1 | LED2 | LED3 | LED4, 0);
+        }
     }
 }
 void sound_change_count_rate(uint16_t new_value){
