5d0728238b669da2c963bde28f21a83efd0ca182
Q-Mart <qumarthjash@gmail.com>
2016-02-29 13:43:07 +0000

    Made every mode use median filter

--- src/multi.c
+++ src/multi.c
@@ -69,7 +69,7 @@ void multi_sweep_loop() {
         pos = pos + scan_direction;
         servo_set_pos(pos);
         timer_delay(35);
-        uint32_t raw = utils_get_ir_and_ultrasound_distance();
+        uint32_t raw = utils_get_ir_and_ultrasound_median_distance();
         network_send(MULTI_SWEEP, &pos, 4, &raw, 4, NULL);     
     }
 
