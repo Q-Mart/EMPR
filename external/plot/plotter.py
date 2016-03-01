bdafc60a2146bc619929cf0bd12eb5bbbac97da6
Oliver Downard <olivermd@btinternet.com>
2016-03-01 11:52:09 +0000

    Multi Mode works with angles that are 0 and 180

--- external/plot/plotter.py
+++ external/plot/plotter.py
@@ -107,34 +107,28 @@ class MultiPlotter(Plotter):
         Plotter.__init__(self, 'Angle', 'Distance', *dimensions)
         self.max_x = 270
         self.max_y = MAX
-        self.xs = []
-        self.ys = []
-        self.centre_x = 180
-        self.centre_y = int(15e5)
+        self.x = []
+        self.y = []
+        self.centre_x = 160
+        self.centre_y = int(150000)
         self._current = 0
 
-    @property
-    def x(self):
-        return self.xs
-
-    @property
-    def y(self):
-        return self.ys
-
     def _append(self, x, y):
-        t = (self._number - self._current - 1) * self._angle
-        t = 0
+        t = (self._current) * self._angle
         cos_t = math.cos(t)
         sin_t = math.sin(t)
 
         # rotate
+        print('----')
+        print(x, y)
         x, y = x - self.centre_x, y - self.centre_y
-        x, y = x*cos_t - y*sin_t, x*sin_t + y*cos_t
+        print(x, y)
+        x, y = (x*cos_t) - (y*sin_t), (x*sin_t) + (y*cos_t)
+        print(x, y)
         x, y = x + self.centre_x, y + self.centre_y
         print(x, y)
-
-        self.xs.append(x)
-        self.ys.append(y)
+        self.x.append(x)
+        self.y.append(y)
 
     def update(self, *data):
         msg, value = data
@@ -142,9 +136,11 @@ class MultiPlotter(Plotter):
             x, y = value
             self._append(x, y)
         elif msg == MultiPlotter.NEXT:
+            print "Next"
             # rotate
-            self._current += 0
+            self._current += 1
         elif msg == MultiPlotter.PARAMS:
             scan_number, _, _ = value
             self._number = scan_number
-            self._angle = math.pi / float(scan_number)
+            self._angle = 2*math.pi / float(scan_number)
+            print "Parameters: ", self._number, self._angle
