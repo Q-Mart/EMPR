5e3f5e6e41866664b2a0e1ba45b65e45106df543
Oliver Downard <omd505@york.ac.uk>
2016-03-08 10:52:29 +0000

    Added ability to view the seperate values of the sensors, needs testing

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,6 @@
 #include "utils.h"
+uint32_t utils_last_ir = 0;
+uint32_t utils_last_us = 0;
 
 void utils_process_digit_input(int last_key_press, uint32_t* result){
     if (last_key_press >= 0){
@@ -28,6 +30,12 @@ uint32_t utils_get_ir_and_ultrasound_distance() {
     uint32_t raw = (raw_ir + raw_us) /2;
     return raw;
 }
+uint32_t utils_get_last_ir_reading(){
+    return utils_last_ir;
+}
+uint32_t utils_get_last_us_reading(){
+    return utils_last_us;
+}
 
 uint32_t utils_get_ir_and_ultrasound_median_distance() {
     int i;
@@ -52,6 +60,8 @@ uint32_t utils_get_ir_and_ultrasound_median_distance() {
     //Get median values
     raw_us = ultrasound_samples[SAMPLE_SIZE/2];
     raw_ir = ir_samples[SAMPLE_SIZE/2];
+    utils_last_us = raw_us;
+    utils_last_ir = raw_ir;
 
     result = (raw_ir + raw_us) / 2;
 
